a76535fa68abbbb5053fc6f3e16cfd3e259b4b74
erraticgenerator <73750234+erraticgenerator@users.noreply.github.com>
2021-01-01 11:59:41 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import React from "react"
 const IndexPage = () => (
   <>
     <h1>My Travel Blog</h1>
+    <p>This is a demo site for <a href="https://erraticgenerator.com/blog/gatsby-blog-with-mapbox-gl-js-using-react-hooks">my blog post</a>.</p>
+    <p>You can also check the <a href="https://github.com/erraticgenerator/gatsby-mapbox-blog">Github repo</a> (made with Gatsbyjs + Mapbox GL JS</p>
   </>
 )
 
